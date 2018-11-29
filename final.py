7bfdc5cf95d7c983405932226cb074f44b95188e
rajeev <rajeevpnair@outlook.com>
2018-11-29 12:01:59 +0530

    Create final.py

---
+++ final.py
@@ -0,0 +1,36 @@
+import win32com.client
+import time
+import os
+import pandas as pd
+import xlrd
+from pandas import ExcelWriter
+
+mail_subject = "subject"
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+inbox = outlook.GetDefaultFolder(6)
+inbox_sub = inbox.Folders("testbox")
+messages = inbox_sub.Items
+message = messages.GetLast()
+subject = message.Subject
+today = time.strftime('%Y-%m-%d')
+received_date = message.ReceivedTime.strftime("%Y-%m-%d")
+
+for message in list(messages):
+	subject = message.Subject
+	received_date = message.ReceivedTime.strftime("%Y-%m-%d")
+	attachments = message.attachments
+	attachment = attachments.Item(1)
+	if subject == mail_subject and received_date == today:
+		attachment.SaveASFile(os.getcwd() + '\\' + str(attachment))
+		df = pd.read_excel('Book1.xlsx' , sheet_name='Sheet1')
+		Job_status = ['Failed', 'Partially Successful']
+		failed_jobs = df[[x in Job_status for x in df['Job Status']]]
+		print (failed_jobs)
+		break
+
+#Delete the files from the local system
+path = os.getcwd()
+files = os.listdir(path)
+for item in files:
+	if item.endswith("xlsx"):
+		os.remove(os.path.join(path, item))
